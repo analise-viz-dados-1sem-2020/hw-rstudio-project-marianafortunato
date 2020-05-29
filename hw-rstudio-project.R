384044c0cd9401221f830e7333b26d8c17832f25
marianafortunato <65910647+marianafortunato@users.noreply.github.com>
2020-05-29 20:13:24 -0300

    Update hw-rstudio-project.R

--- hw-rstudio-project.R
+++ hw-rstudio-project.R
@@ -2,7 +2,7 @@ library(magrittr); library(ggplot2)
 
 source("R/utils.R")
 
-x <- "Franscisco"
+x <- "Francisco"
 
 df <- get_freq_nome(x)
 
@@ -14,3 +14,4 @@ df %>%
   xlab("Década de nascimento") +
   ylab("Pessoas") +
   ggsave("figures/nomes-decada.pdf")
+© 2020 GitHub, Inc.
