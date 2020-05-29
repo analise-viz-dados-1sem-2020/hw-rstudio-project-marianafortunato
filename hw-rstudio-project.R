476502d2781bc6cb67e2daaae32bed45f7cc2609
marianafortunato <65910647+marianafortunato@users.noreply.github.com>
2020-05-29 19:43:13 -0300

    Update hw-rstudio-project.R

--- hw-rstudio-project.R
+++ hw-rstudio-project.R
@@ -1,16 +1,6 @@
-library(magrittr); library(ggplot2)
+x <- 5
+pdf(*figure/viz.pdf*)
 
-source("R/utils.R")
+plot(1x)
 
-x <- "Francisco"
-
-df <- get_freq_nome(x)
-
-df %>% 
-  ggplot(aes(x = decada, y = freq)) + 
-  geom_point() + 
-  geom_line(aes(group = 1)) +
-  labs(title = paste("Nascimentos por década", x, sep = " - ")) +
-  xlab("Década de nascimento") +
-  ylab("Pessoas") +
-  ggsave("figures/nomes-decada.pdf")
+dev.off()
